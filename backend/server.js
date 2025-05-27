021b13803dd9ea03df8bf8f1e81dd756ccd57fa4
falaprodavi <falaprodavo@gmail.com>
2025-05-27 12:19:09 -0300

    cors update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 const { protect, authorize } = require("./middleware/auth");
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://app-negocios-locais.vercel.app", // permite só seu frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Servir arquivos estáticos (para as imagens)
